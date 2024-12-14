2489d2fef32b8f1ad9ef655c4cf12d0a324cc72e
Susanna Maria Hepp <susanna@olsoni.de>
2024-12-14 22:57:07 +0100

    0001 small change

--- application/updater/feature_extractor.py
+++ application/updater/feature_extractor.py
@@ -22,6 +22,7 @@ def get_tracks_without_features(cursor):
 
 
 def run_essentia_extractor(input_file):
+    logger.info(f"analyse {input_file}")
     # Initialize the Docker client
     client = docker.from_env()
 
